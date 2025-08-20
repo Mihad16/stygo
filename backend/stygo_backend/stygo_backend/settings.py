1732d5c915349ca29fb3e44911cd4b4a57207ccd
Mihad Bornio <aboobackermihad@gmail.com>
2025-08-20 21:19:24 +0530

    update all

--- backend/stygo_backend/stygo_backend/settings.py
+++ backend/stygo_backend/stygo_backend/settings.py
@@ -101,10 +101,15 @@ WSGI_APPLICATION = 'stygo_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',          # or your db name
+        'USER': 'postgres',
+        'PASSWORD': 'mihadql9605',
+        'HOST': 'localhost',
+        'PORT': '5433',              # <-- changed here
     }
 }
+
 
 
 # Password validation
